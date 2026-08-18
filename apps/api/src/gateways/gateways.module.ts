c20d415f1e80ca7a49c70389cab9f4d84c1f19ae
Jitendra kumar <jitendrasaw148@gmail.com>
2026-08-18 07:41:53 +0530

    fix: add websocket user repository

--- apps/api/src/gateways/gateways.module.ts
+++ apps/api/src/gateways/gateways.module.ts
@@ -3,12 +3,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AmbulanceDriverEntity } from '../database/entities/ambulance-driver.entity';
 import { EmergencyRequestEntity } from '../database/entities/emergency-request.entity';
+import { UserEntity } from '../database/entities/user.entity';
 import { MedlinkGateway } from './medlink.gateway';
 
 @Module({
   imports: [
     JwtModule.register({}),
-    TypeOrmModule.forFeature([AmbulanceDriverEntity, EmergencyRequestEntity]),
+    TypeOrmModule.forFeature([AmbulanceDriverEntity, EmergencyRequestEntity, UserEntity]),
   ],
   providers: [MedlinkGateway],
   exports: [MedlinkGateway],
