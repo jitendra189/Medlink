1a0bbbae64dd714eb5386cd65bd101c423491dc6
Jitendra kumar <jitendrasaw148@gmail.com>
2026-08-19 20:34:54 +0530

    fix(storage): inject centralized file storage into users module

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../database/entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UploadsModule } from '../uploads/uploads.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), UploadsModule],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
